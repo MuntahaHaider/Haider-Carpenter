d00073085c31bee285fcc92a069fd29f1bc3d714
MuntahaHaider <muntahahaiderali222@gmail.com>
2024-05-25 13:24:04 +0500

    haider carpenter

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function home(){
+    var homePage = document.getElementById('home')
+     homePage.href = "home.html"
+ }
+ function about(){
+     var aboutPage = document.getElementById('about')
+     aboutPage.href = "about.html"
+ }
+ function category(){
+     var categoryPage = document.getElementById('blog')
+     categoryPage.href = "blog.html"
+ }
+ function contact(){
+     var contactPage = document.getElementById('contact')
+     contactPage.href = "contact.html"
+ }
+ function mouseOver(){
+     var card1 = document.getElementById('card1')
+     card1.style.backgroundColor = "rgba(0, 0, 0, 0.5);"
+     card1.innerHTML = "Wooden work"
+ }
+ function mouseOver(){
+     var card1 = document.getElementById('card1')
+     card1.src = "img/WhatsApp Image 2024-05-11 at 17.27.01_e7f418db.jpg"
+ }
+ 
